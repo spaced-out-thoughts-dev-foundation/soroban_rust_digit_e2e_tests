ace80d8ad61fc155df59a60ae6eb40d12b6b01a4
Rob Durst <me@robdurst.com>
2024-07-12 18:20:13 -0600

    move from monorepo

---
+++ stellar_soroban/official_sdf_examples/alloc/src/lib.rs
@@ -0,0 +1,30 @@
+#![no_std]
+use soroban_sdk::{
+    auth::Context, contract, contractimpl, unwrap::UnwrapOptimized, vec, Env, IntoVal,
+};
+
+#[contract]
+pub struct AllocContract;
+
+#[contractimpl]
+impl AllocContract {
+    pub fn sum(_env: Env, count: i128) -> i128 {
+        let Thing_to_return: i128;
+        let mut v1 = vec![];
+        let mut METHOD_CALL_EXPRESSION_7 = 0..count;
+        METHOD_CALL_EXPRESSION_7.for_each(v1.push(&i));
+        let mut sum = 0;
+        let mut ITERATOR_15 = v1;
+        let mut i = start(&v1);
+        if !iteration_finished {
+            if CHECK_CONDITION_ASSIGNMENT_16 {
+                sum = sum + i;
+                increment += i;
+                // goto: 19
+            }
+        }
+        return sum;
+    }
+}
+
+mod test;
